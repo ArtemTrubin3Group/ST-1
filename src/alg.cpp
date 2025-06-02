5ade4dd73bcce11cb239daf7af6db772008e1b7f
ArtemTrubin3Group <atrubin41@gmail.com>
2025-06-02 10:39:38 +0300

    fix code

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,15 @@
 // Copyright 2025 UNN-CS
-#include <cstdint>
 #include "alg.h"
+#include <cstdint>
+#include <cmath>
+#include <vector>
 
 
 bool checkPrime(uint64_t value) {
-  if (value < 2) return false;
-  if (value == 2) return true;
-  if (value % 2 == 0) return false;
-
-  for (uint64_t i = 3; i * i <= value; i += 2) {
+  if (value <= 1) {
+    return false;
+  }
+  for (uint64_t i = 2; i <= std::sqrt(value); i++) {
     if (value % i == 0) {
       return false;
     }
@@ -19,38 +20,35 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
   if (n == 0) return 0;
 
-  uint64_t count = 0;
-  uint64_t num = 2;
+  uint64_t primes = 0;
+  uint64_t current = 2;
 
-  while (count < n) {
-    if (checkPrime(num)) {
-      count++;
+  while (primes < n) {
+    if (checkPrime(current)) {
+      primes++;
+      if (primes == n) {
+        return current;
+      }
     }
-    num++;
+    current++;
   }
-
-  return num - 1;
+  return current;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  uint64_t num = value + 1;
-
-  while (true) {
-    if (checkPrime(num)) {
-      return num;
-    }
-    num++;
+  uint64_t nextNum = value + 1;
+  while (!checkPrime(nextNum)) {
+    nextNum++;
   }
+  return nextNum;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-
   for (uint64_t i = 2; i < hbound; i++) {
     if (checkPrime(i)) {
       sum += i;
     }
   }
-
   return sum;
 }
